1c4c7828da009df7402b7ba4bc686d7e73b7bb0b
itisjiraya <itsjiraya@outlook.com>
2024-07-23 11:17:03 +0300

    ain

---
+++ src/utils/agreement.py
@@ -0,0 +1,63 @@
+import os
+import msvcrt
+from src.utils.utils import _utils
+import sys
+
+class Agreement:
+    def __init__(self):
+        self.clear_screen = os.system("cls")
+
+    def check_files(self):
+        self.clear_screen()
+        if os.path.exists("EULA.txt"):
+            with open("EULA.txt", "r") as eulaFile:
+                if "The project is subject to the MIT license.\nEULA=True" == eulaFile.read():
+                    return _utils.token_request()
+                
+                else:
+                    return self.license_confirmation()
+                
+        else:
+            open("EULA.txt", "w+").write("The project is subject to the MIT license.\nEULA=False")
+            return self.license_confirmation()
+
+    def open_license(self):
+        self.clear_screen()
+
+
+        with open("LICENSE", "r") as file:
+            print(file.read())
+        print("\nPress any key to continue...")
+        msvcrt.getch()
+        return self.license_confirmation()
+
+    def license_confirmation(self):
+        self.clear_screen()
+        print("Please confirm your agreement with the terms of use license.\n\n1. Accept\n2. Decline\n3. Review\n0. Exit")
+
+        
+        user_input = _utils.input_request(["1", "2", "3", "0"])
+            
+        if user_input == "1":
+            with open("EULA.txt", "w") as eulaFile:
+                eulaFile.write("The project is subject to the MIT license.\nEULA=True")
+
+            return _utils.token_request()
+
+        elif user_input == "2":
+            with open("EULA.txt", "w") as eulaFile:
+                eulaFile.write("The project is subject to the MIT license.\nEULA=False")
+            print("License is mandatory for acceptance. ^-^")
+            sys.exit()
+
+        elif user_input == "3":
+            self.open_license()
+            return self.license_confirmation()
+
+        else:
+            sys.exit()
+
+
+_agreement = Agreement()
+
+_agreement.license_confirmation()
---
+++ src/utils/utils.py
@@ -0,0 +1,47 @@
+import os
+import msvcrt
+import sys
+from getpass import getpass
+from agreement import _agreement
+from typing import Optional, List
+
+class Utils:
+    def input_request(self, available_answers: Optional[List[str]] = None) -> str:
+        if available_answers is None:
+            return msvcrt.getch().decode()
+
+        while True:
+            user_input = msvcrt.getch().decode()
+            if user_input in available_answers:
+                break
+
+        return user_input
+
+    def token_request(self):
+        os.system("cls")
+        print("Please provide the Discord bot token\nYou can find the instructions in the README.md and by pressing '2'.\n1. Enter bot token\n2. Instructions\n3. Edit EULA\n0. Exit")
+
+        user_input = self.input_request(["1", "2", "3", "0"])
+
+        if user_input == "1":
+            secret = getpass("Enter bot token: (HIDDEN)")
+            return self.validation_check(secret=secret)
+
+        elif user_input == "2":
+            # TODO: Make an instruction with links :D
+            return NotImplemented
+
+        elif user_input == "3":
+            return _agreement.license_confirmation()
+
+        elif user_input == "0":
+            sys.exit()
+
+        else:
+            return self.token_request()
+
+    def validation_check(self, secret):
+        # TODO: Method logic
+        return NotImplemented
+
+_utils = Utils()
