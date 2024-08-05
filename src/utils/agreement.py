2d24e2e7594736f26c0b7248221de8509bf0af2b
itisjiraya <itsjiraya@outlook.com>
2024-08-05 19:30:31 +0300

    Ну чота сделал. Добавил выбор языка

--- src/utils/agreement.py
+++ src/utils/agreement.py
@@ -1,31 +1,38 @@
 import os
 import msvcrt
-from src.utils.utils import _utils
+from src.utils import utils
 import sys
+import gettext
 
-class Agreement:    
-    """
-    class
-    """
-    def __init__(self):
-        self.clear_screen = os.system("cls")
+EULA_FILE = "EULA.txt"
 
+
+class Agreement:
     def check_eula_file(self):
-        self.clear_screen()
-        if os.path.exists("EULA.txt"):
-            with open("EULA.txt", "r") as eulaFile:
+        """
+        Check if the EULA.txt file exists and if it contains the correct license.
+        """
+        os.system("cls")
+        if os.path.exists(EULA_FILE):
+            with open(EULA_FILE, "r") as eulaFile:
                 if "The project is subject to the MIT license.\nEULA=True" == eulaFile.read():
-                    return _utils.token_request()
-                
+                    return utils.utils_instance.token_request()
+
                 else:
                     return self.license_confirmation()
-                
+
         else:
-            open("EULA.txt", "w+").write("The project is subject to the MIT license.\nEULA=False")
+            open(EULA_FILE, "w+").write("The project is subject to the MIT license.\nEULA=False")
             return self.license_confirmation()
 
     def open_license(self):
-        self.clear_screen()
+        """
+        Opens the LICENSE file and prints its contents to the console.
+        Clears the console screen before displaying the license.
+        Prompts the user to press any key to continue.
+        Returns the result of the license_confirmation method.
+        """
+        os.system("cls")
         with open("LICENSE", "r") as file:
             print(file.read())
         print("\nPress any key to continue...")
@@ -33,20 +40,25 @@ class Agreement:
         return self.license_confirmation()
 
     def license_confirmation(self):
-        self.clear_screen()
-        print("Please confirm your agreement with the terms of use license.\n\n1. Accept\n2. Decline\n3. Review\n0. Exit")
+        """
+        Prompts the user to confirm their agreement with the terms of use license.
 
-        
-        user_input = _utils.input_request(["1", "2", "3", "0"])
-            
+        This function displays a menu with options to accept, decline, review, or exit.
+        The user's input is validated and the appropriate action is taken based on the input.
+        """
+        os.system("cls")
+        print("Please confirm your agreement with the TOS license.\n\n1. Accept\n2. Decline\n3. Review\n0. Exit")
+
+        user_input = utils.Utils.input_request(["1", "2", "3", "0"])
+
         if user_input == "1":
-            with open("EULA.txt", "w") as eulaFile:
+            with open(EULA_FILE, "w") as eulaFile:
                 eulaFile.write("The project is subject to the MIT license.\nEULA=True")
 
-            return _utils.token_request()
+            return utils.utils_instance.token_request()
 
         elif user_input == "2":
-            with open("EULA.txt", "w") as eulaFile:
+            with open(EULA_FILE, "w") as eulaFile:
                 eulaFile.write("The project is subject to the MIT license.\nEULA=False")
             print("License is mandatory for acceptance. ^-^")
             sys.exit()
@@ -59,6 +71,4 @@ class Agreement:
             sys.exit()
 
 
-_agreement = Agreement()
-
-_agreement.license_confirmation()
+agreement_instance = Agreement()
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,53 +1,120 @@
+import json
 import os
 import msvcrt
 import sys
 from getpass import getpass
-from agreement import _agreement
+from src.utils import agreement
 from typing import Optional, List
+import gettext
+
 
 class Utils:
-    """
-    class
-    """
-    def __inits__(self):
-        self.clear_screen = os.system("cls")
-        
-    def input_request(self, available_answers: Optional[List[str]] = None) -> str:
-        if available_answers is None:
-            return msvcrt.getch().decode()
+    def __init__(self):
+        self._ = gettext.gettext
+
+    @staticmethod
+    def input_request(available_answers: Optional[List[str]] = None) -> str:
+        """
+        Requests user input from the console and returns it as a string.
+
+        This method waits for a single character input. If the `available_answers` parameter is provided, the method
+        will only return a value if matches one of the specified valid answers. If `available_answers` is not provided
+        or is an empty list, the method will accept any character input.
+
+        :param available_answers: An optional list of valid answers. If provided, the method will only return a value
+                                 that matches one of the answers in this list. If not provided or if the list is empty,
+                                 any character input will be accepted.
+
+        :return: The user's input as a string, which will either be a valid answer from `available_answers` or character
+                 if no valid answers are specified.
+        """
+        if not available_answers:  # Accept any character input if no valid answers are specified.
+            while True:
+                try:
+                    return msvcrt.getch().decode()  # Wait for a character input and decode it.
+                except UnicodeDecodeError:  # Ignore non-string inputs (e.g., arrow keys).
+                    continue
 
         while True:
-            user_input = msvcrt.getch().decode()
-            if user_input in available_answers:
-                break
-
-        return user_input
+            try:
+                user_input = msvcrt.getch().decode()  # Wait for a character input and decode it.
+                if user_input in available_answers:  # Check if the input is one of the valid answers.
+                    return user_input  # Return the valid input.
+            except UnicodeDecodeError:  # Ignore non-string inputs (e.g., arrow keys).
+                continue
 
     def token_request(self):
-        os.system("cls")
-        print("Please provide the Discord bot token\nYou can find the instructions in the README.md and by pressing '2'.\n1. Enter bot token\n2. Instructions\n3. Edit EULA\n0. Exit")
+        """
+        Requests the Discord bot token from the console.
 
-        user_input = self.input_request(["1", "2", "3", "0"])
+        This method presents the user with options to either enter the bot token manually, view instructions,
+        edit the End User License Agreement (EULA), or exit the program. The user can choose from the following options:
+
+        - '1': Enter the bot token manually.
+        - '2': View instructions for obtaining the token.
+        - '3': Edit the EULA.
+        - '0': Exit the program.
+
+        :return: The Discord bot token as a string if the user chooses to enter it manually.
+                 If the user selects options '2', '3', or '0', the method will not return a token.
+        """
+        os.system("cls")  # Clear the console screen for better readability.
+        print("Please provide the Discord bot token\nYou can find the instructions in the README.md and by pressing "
+              "'2'.\n\n1. Enter bot token\n2. Instructions\n3. Edit EULA\n0. Exit")
+
+        user_input = self.input_request(["1", "2", "3", "0"])  # Get user input for the desired action.
 
         if user_input == "1":
-            secret = getpass("Enter bot token: (HIDDEN)")
-            return self.validation_check(secret=secret)
+            secret = getpass("Enter bot token: (HIDDEN)").strip()  # Prompt for the bot token, keeping it hidden.
+            print(secret)
+            return self.validation_check(secret=secret)  # Validate and return the entered token.
 
         elif user_input == "2":
-            # TODO: Make an instruction with links :D
-            return NotImplemented
+            # TODO: Implement instructions with relevant links.
+            return NotImplemented  # Placeholder for future implementation.
 
         elif user_input == "3":
-            return _agreement.license_confirmation()
+            return agreement.agreement_instance.license_confirmation()  # Handle EULA editing.
 
         elif user_input == "0":
-            sys.exit()
+            sys.exit()  # Exit the program.
 
         else:
             return self.token_request()
 
-    def validation_check(self, secret):
+    def validation_check(self, secret: str):
         # TODO: Method logic
         return NotImplemented
 
-_utils = Utils()
+    def language_selection(self) -> None:
+        """
+        Selects the language of the program and saves it to a JSON file.
+
+        This method displays a menu with options to select the language. The user's input is validated and the
+        appropriate action is taken based on the input. The selected language is saved to a JSON file.
+
+        :return: None
+        """
+        os.system("cls")  # Clear the console screen for better readability.
+        print("Select language | Выберите язык\n\n1. English\n2. Русский\n0. Exit | Выход")
+
+        user_input = self.input_request(["1", "2", "0"])  # Get user input for the desired action.
+
+        settings_dir = "./settings/"
+        if not os.path.exists(settings_dir):
+            os.makedirs(settings_dir)
+
+        if user_input == "1":
+            with open(os.path.join(settings_dir, "settings.json"), "w") as settings_file:
+                json.dump({"language": "en"}, settings_file, indent=4)
+                return
+
+        elif user_input == "2":
+            with open(os.path.join(settings_dir, "settings.json"), "w") as settings_file:
+                json.dump({"language": "ru"}, settings_file, indent=4)
+                return
+
+        sys.exit()  # Exit the program.
+
+
+utils_instance = Utils()
